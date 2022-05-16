7e4ecd8f48e30e7e7ac03de2e4f7a9d00d30e2e9
Abhyudaya Kandwal <abhyudayakandwal001@gmail.com>
2022-05-16 10:51:03 +0530

    day5

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -1,23 +1,32 @@
 package com.example.a7minutesworkout
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var binding:ActivityExerciseBinding?=null
     private var myTimer:CountDownTimer?=null
     private var myTime=10
     private var exNo=1
     private var exList:ArrayList<ExerciseModel>?=null
+    private var player:MediaPlayer?=null
+    private var tts:TextToSpeech?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityExerciseBinding.inflate(layoutInflater)
         setContentView(binding?.root)
         setSupportActionBar(binding?.toolbarExercise)
+        tts= TextToSpeech(this,this)
         binding?.tvTimer?.text=myTime.toString()
         if(supportActionBar!=null){
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -29,9 +38,13 @@ class ExerciseActivity : AppCompatActivity() {
         timer()
     }
     private fun timer() {
+        binding?.upcExName?.text="${exList!![exNo-1].getName()}"
+        speak("Get ready for ${exList!![exNo-1].getName()}")
+        myTime=10;
         myTimer=object:CountDownTimer(10000,1000) {
             override fun onTick(p0: Long) {
                 myTime--
+//                if(myTime<=3)speak(myTime.toString())
                 binding?.restpb?.progress=myTime
                 binding?.resttv?.text=myTime.toString()
             }
@@ -39,6 +52,8 @@ class ExerciseActivity : AppCompatActivity() {
             override fun onFinish() {
                 binding?.flrestpb?.visibility=View.INVISIBLE
                 binding?.rvtitle?.visibility=View.INVISIBLE
+                binding?.upcex?.visibility=View.INVISIBLE
+                binding?.upcExName?.visibility=View.INVISIBLE
                 binding?.flprogressbar?.visibility=View.VISIBLE
                 binding?.tvTitle?.visibility=View.VISIBLE
                 binding?.exImg?.visibility=View.VISIBLE
@@ -50,11 +65,23 @@ class ExerciseActivity : AppCompatActivity() {
         }.start()
     }
     private fun exerciseTimer() {
-        var cd:CountDownTimer?=null
+
         var cdTime=25;
-        cd=object :CountDownTimer(25000,1000){
+        speak("Start")
+        myTimer=object :CountDownTimer(25000,1000){
             override fun onTick(p0: Long) {
                 cdTime--
+            if(cdTime==5){
+                try{
+                    val soundURI= Uri.parse("android.resource://com.example.a7minutesworkout/"+R.raw.count_down)
+                    player=MediaPlayer.create(applicationContext,soundURI)
+                    player?.isLooping=false
+                    player?.start()
+                }catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+//                speak(cdTime.toString())
                 binding?.progressbar?.progress=cdTime
                 binding?.tvTimer?.text=cdTime.toString()
             }
@@ -62,9 +89,15 @@ class ExerciseActivity : AppCompatActivity() {
             override fun onFinish() {
                 exNo++
                 if(exNo<=3){
-                    binding?.exImg?.setImageResource(exList!![exNo-1].getImage())
-                    binding?.tvTitle?.text=exList!![exNo-1].getName()
-                    exerciseTimer()
+                    binding?.flrestpb?.visibility=View.VISIBLE
+                    binding?.rvtitle?.visibility=View.VISIBLE
+                    binding?.upcex?.visibility=View.VISIBLE
+                    binding?.upcExName?.visibility=View.VISIBLE
+                    binding?.flprogressbar?.visibility=View.INVISIBLE
+                    binding?.tvTitle?.visibility=View.INVISIBLE
+                    binding?.exImg?.visibility=View.INVISIBLE
+                    binding?.progressbar?.max=10
+                    timer()
                 }else{
                     Toast.makeText(this@ExerciseActivity,"Finished",Toast.LENGTH_SHORT).show()
                 }
@@ -72,9 +105,33 @@ class ExerciseActivity : AppCompatActivity() {
 
         }.start()
     }
+    private fun speak(text:String){
+        tts?.speak(text,TextToSpeech.QUEUE_FLUSH,null,"")
+    }
 
+    override fun onInit(status: Int) {
+       if(status==TextToSpeech.SUCCESS){
+           val result=tts!!.setLanguage(Locale.US)
+           if(result==TextToSpeech.LANG_MISSING_DATA||result==TextToSpeech.LANG_NOT_SUPPORTED){
+               Log.e("TTS","Initialization failed")
+           }
+       }else{
+           Log.e("TTS","Initialize failed")
+       }
+    }
     override fun onDestroy() {
         super.onDestroy()
+        if(player!=null){
+            player!!.stop()
+        }
+        if(myTimer!=null){
+            myTimer!!.cancel()
+            myTimer=null
+        }
+        if(tts!=null){
+            tts!!.stop()
+            tts!!.shutdown()
+        }
         binding=null
     }
 }
